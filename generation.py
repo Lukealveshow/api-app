c0a825cc39c7e5648089a9e5f6ad5db3a2289edd
Lucas Alves Martins <lucasalvesmartins10@gmail.com>
2025-11-02 21:44:53 -0300

    Pronto para deploy

--- generation.py
+++ generation.py
@@ -6,7 +6,7 @@ def generate_answer(question, context):
     model = AutoModelForQuestionAnswering.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     inputs = tokenizer(question, context, return_tensors="pt")
-    outputs = model(**inputs)
+    outputs = model(inputs)
     answer_start = torch.argmax(outputs.start_logits)
     answer_end = torch.argmax(outputs.end_logits) + 1
     answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end]))
